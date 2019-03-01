c423fcb9feb0d4c82d4c5883fa9342fc870e0b0d
Adeodato Simó <dato@fi.uba.ar>
2019-03-01 03:29:48 -0300

    Script para crear repos.

---
+++ orga_repos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+
+API=https://api.github.com
+
+USER=adeobot
+TOKEN=$(< ~/.adeobot.tok)
+TEAM_SLUG=orga
+
+# TEAM_ID can be found out with:
+#   get orgs/fiubatps/teams | jq '.[] | select(.slug == "orga") | .id'
+TEAM_ID=3141587
+
+api() {
+      local verb="$1"; shift
+      local endpoint="$1"; shift
+      # apt install httpie
+      http -h -a "$USER:$TOKEN" -I "$verb" "$API/$endpoint" "$@"
+}
+
+get() {
+    api GET "$@"
+}
+
+post() {
+    api POST "$@"
+}
+
+put() {
+    api PUT "$@"
+}
+
+while read user repo; do
+    url="https://github.com/fiubatps/$repo"
+    post orgs/fiubatps/repos name:="\"$repo\"" private:=true team_id:="$TEAM_ID"
+    git -C ../orga_alu push "$url" master
+    put "repos/fiubatps/$repo/collaborators/$user"
+done < repos_beta0.txt
