8e3920d4520103bd143ed878e1901218b7df1214
Adeodato Simó <dato@fi.uba.ar>
2019-03-02 14:00:26 -0300

    Armonizar con script común.

--- orga_repos.sh
+++ orga_repos.sh
@@ -4,16 +4,20 @@
 
 set -eu
 
-API=https://api.github.com
+ORG="fiubatps"
+API="https://api.github.com"
 
-USER=adeobot
-TOKEN=$(< ~/.adeobot.tok)
-CHECK_ID=autograder
-TEAM_SLUG=orga
-
-# TEAM_ID can be found out with:
-#   get orgs/fiubatps/teams | jq '.[] | select(.slug == "orga") | .id'
+# El identificador numérico de un equipo "eqx" se puede obtener con:
+#   http -a ... $API/orgs/$ORG/teams | jq '.[] | select(.slug == "eqx") | .id'
 TEAM_ID=3141587
+TEAM_SLUG="orga"
+
+# Autenticación (con API token) para el bot de la administración.
+USER="adeobot"
+TOKEN=$(< ~/.adeobot.tok)
+
+# Otras variables.
+CHECK_ID="autograder"
 
 api() {
       local verb="$1"; shift
@@ -40,31 +44,29 @@ status_checks='{"strict": false, "contexts": ["'"$CHECK_ID"'"]}'
 pull_requests='{"dismiss_stale_reviews": true, "require_code_owner_reviews": true}'
 
 while read user repo; do
-    url="https://github.com/fiubatps/$repo"
+    url="https://github.com/$ORG/$repo"
 
     # Crear el repositorio.
-    post orgs/fiubatps/repos name:="\"$repo\""   \
-                             private:=true       \
-                             has_wiki:=false     \
-                             allow_squash_merge:=false \
-                             allow_rebase_merge:=false
+    post "orgs/$ORG/repos" name:="\"$repo\""   \
+                           private:=true       \
+                           has_wiki:=false     \
+                           allow_squash_merge:=false \
+                           allow_rebase_merge:=false
 
-    # Dar permisos al equipo docente. (En el futuro, si hay múltiples
-    # correctores, otorgar a este equipo acceso "push" en lugar de
-    # "admin", y crear un sub-equipo separado para administradores.)
-    put "teams/$TEAM_ID/repos/fiubatps/$repo" permission:='"admin"'
+    # Dar permisos al equipo docente.
+    put "teams/$TEAM_ID/repos/$ORG/$repo" permission:='"admin"'
 
     # Enviar el esqueleto.
     git push "$url" origin/skel_master:refs/heads/master \
                     origin/skel_entregas:refs/heads/entregas
 
     # Proteger la rama entregas.
-    put "repos/fiubatps/$repo/branches/entregas/protection" \
+    put "repos/$ORG/$repo/branches/entregas/protection" \
          enforce_admins:=false                    \
          restrictions:="$entregas_push"           \
          required_status_checks:="$status_checks" \
          required_pull_request_reviews:="$pull_requests"
 
     # Enviar la invitación.
-    put "repos/fiubatps/$repo/collaborators/$user"
+    put "repos/$ORG/$repo/collaborators/$user"
 done < repos_beta1.txt
